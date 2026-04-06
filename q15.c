0328419904b3eaba3ce625828c1c8c11896d1fa7
ArnikaGupta <arnikaguptaaa@gmail.com>
2026-04-06 20:35:13 +0530

    Add files via upload

---
+++ q15.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int m, n;
+    scanf("%d %d", &m, &n);
+
+    int matrix[m][n];
+
+    // Input matrix
+    for(int i = 0; i < m; i++) {
+        for(int j = 0; j < n; j++) {
+            scanf("%d", &matrix[i][j]);
+        }
+    }
+
+    int sum = 0;
+
+    // Sum of primary diagonal
+    for(int i = 0; i < m && i < n; i++) {
+        sum += matrix[i][i];
+    }
+
+    // Output result
+    printf("%d", sum);
+
+    return 0;
+}
